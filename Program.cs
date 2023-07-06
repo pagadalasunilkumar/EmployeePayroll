8d406aa2345e8dd8e4fc6a2e4b11e6bb4594ae93
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-07-06 12:23:49 +0530

    [sunil ] the file is added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EmployeePayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=localhost;Initial Catalog=payroll_service;User ID=your-username;Password=your-password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Connection established
+                    Console.WriteLine("Connected to the database.");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error connecting to the database: " + ex.Message);
+                }
+
+            }
+        }
+    }
+}
---
+++ UC-1CreatePayroll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    internal class UC_1CreatePayroll
+    {
+        public static void createdatabase()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"data source=DESKTOP-SAGJTNV\SQLEXPRESS;initial catalog=master;integrated security=true");
+                con.Open();
+                string query = "create database payroll_service;";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database created Succesfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
